a1ba5b4832ffb5c7845aa6273772e66e1b884985
Семен <semenlo@inbox.ru>
2025-01-25 00:24:04 +0500

    Добавил кучу параметров + вроде все что нужно для тренеровки модели, но нужно добавить ещё параметров на вывод

--- main.py
+++ main.py
@@ -1,26 +1,85 @@
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import QDir
 
+import regression
+
+
 if __name__ == '__main__':
+
     app = QApplication([])
     main = QWidget()
     line = QVBoxLayout()
-    main.setGeometry(600, 500, 600, 500)
+    main.setGeometry(600,500,600,500)
 
     btn_browse = QPushButton('Выбрать папку')
     btn_browse2 = QPushButton('Выбрать файл')
+
+    file_name_csv = QLabel('')
+    lbl_data = QLabel('Данные:')
+    lbl_model_param = QLabel('Модель:')
+
+    get_y_name = QLineEdit()
+    get_y_name.setPlaceholderText('Название целевой переменной')
+    get_y_name.setToolTip('Переменная, которую вы будете предсказывать')
+
+    if_shuffle  = QCheckBox(' Перемешивать список при разделении данных ')
+    percent_data = QLineEdit()
+    percent_data.setPlaceholderText('% тестовых данных')
+    percent_data.setToolTip('Процент тестовых данных в %')
+
     choose_task = QComboBox()
     choose_task.addItems(['Задача регрессии (CSV Файл)', 'Задача классификации (CSV Файл)', 'Задача классификации (Изображения)'])
+    choose_model_regression = QComboBox()
+    choose_model_regression.addItems(['RandomForestRegressor','GradientBoostingRegressor','LinearRegression'])
+
+    get_reg_forest_estimators = QLineEdit()
+    get_reg_forest_estimators.setPlaceholderText('n_estimators')
+    get_reg_forest_estimators.setToolTip('Количество случайных деревьев')
+    get_reg_forest_max_depth = QLineEdit()
+    get_reg_forest_max_depth.setPlaceholderText('max_depth')
+    get_reg_forest_max_depth.setToolTip('Максимальная глубина дерева')
+    get_reg_forest_min_samples_leaf = QLineEdit()
+    get_reg_forest_min_samples_leaf.setPlaceholderText('min_samples_leaf')
+    get_reg_forest_min_samples_leaf.setToolTip('Минимальное кол-во объектов в листе дерева')
+    get_reg_forest_min_samples_split = QLineEdit()
+    get_reg_forest_min_samples_split.setPlaceholderText('min_samples_split')
+    get_reg_forest_min_samples_split.setToolTip('Минимальное кол-во признаков для разбиения')
+    get_reg_forest_max_features = QLineEdit()
+    get_reg_forest_max_features.setPlaceholderText('max_features')
+    get_reg_forest_max_features.setToolTip('Максимальное количество признаков')
+    get_reg_forest_max_samples = QLineEdit()
+    get_reg_forest_max_samples.setPlaceholderText('max_samples')
+    get_reg_forest_max_samples.setToolTip('Максимальное количество образцов')
+
+    btn_browse2.setVisible(True)
+    btn_browse.setVisible(False)
 
     row1 = QHBoxLayout()
     row2 = QHBoxLayout()
+    row3 = QHBoxLayout()
+    row23 = QHBoxLayout()
+    row223 = QHBoxLayout()
+    row4 = QHBoxLayout()
+    row5 = QHBoxLayout()
+    row6 = QHBoxLayout()
+    row7 = QHBoxLayout()
 
     row1.addWidget(choose_task)
     row2.addWidget(btn_browse)
     row2.addWidget(btn_browse2)
-
-    btn_browse2.setVisible(True)
-    btn_browse.setVisible(False)
+    row223.addWidget(file_name_csv)
+    row23.addWidget(lbl_data)
+    row3.addWidget(get_y_name)
+    row3.addWidget(percent_data)
+    row3.addWidget(if_shuffle)
+    row4.addWidget(lbl_model_param)
+    row5.addWidget(choose_model_regression)
+    row6.addWidget(get_reg_forest_estimators)
+    row6.addWidget(get_reg_forest_max_depth)
+    row6.addWidget(get_reg_forest_min_samples_leaf)
+    row7.addWidget(get_reg_forest_min_samples_split)
+    row7.addWidget(get_reg_forest_max_features)
+    row7.addWidget(get_reg_forest_max_samples)
 
     def test_train_data_find():
         # Test will be same
@@ -30,12 +89,14 @@ if __name__ == '__main__':
             print(directory_train)
 
     def csv_file_finder():
+
         global directory_csv
         directory_csv, _ = QFileDialog.getOpenFileName(main, "Выбрать файл", QDir.homePath(), "Все файлы (*.*)")
         if directory_csv:
-            print(directory_csv)
+            file_name_csv.setText(f'CSV файл - {directory_csv}')
 
     def change_task(index):
+
         if index == 0:
             btn_browse2.setVisible(True)
             btn_browse.setVisible(False)
@@ -48,12 +109,31 @@ if __name__ == '__main__':
             btn_browse2.setVisible(False)
             btn_browse.setVisible(True)
 
+    def change_model_regression(index):
+
+        if index == 0:
+            pass
+
+        elif index == 1:
+            pass
+
+        elif index == 3:
+            pass
+
     btn_browse.clicked.connect(test_train_data_find)
     btn_browse2.clicked.connect(csv_file_finder)
     choose_task.currentIndexChanged.connect(change_task)
+    choose_model_regression.currentIndexChanged.connect(change_model_regression)
 
     line.addLayout(row1)
     line.addLayout(row2)
+    line.addLayout(row223)
+    line.addLayout(row23)
+    line.addLayout(row3)
+    line.addLayout(row4)
+    line.addLayout(row5)
+    line.addLayout(row6)
+    line.addLayout(row7)
 
     main.setLayout(line)
     main.show()
--- regression.py
+++ regression.py
@@ -1,6 +1,9 @@
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+
+import pandas as pd
 
 def random_forest(**kwargs):
     return RandomForestRegressor(kwargs)
@@ -13,32 +16,50 @@ def gradient_boosting(**kwargs):
 
 def params_random_forest(a,b,c,d,e,f):
     return {
-        'n_estimators':[a],
-        'max_depth':[b],
-        'min_samples_leaf':[c],
-        'min_samples_split':[d],
-        'max_features':[e],
-        'max_samples':[f]
+        'n_estimators':a,
+        'max_depth':b,
+        'min_samples_leaf':c,
+        'min_samples_split':d,
+        'max_features':e,
+        'max_samples':f
         }
 
 def params_line_regression(a,b,c,d):
     return {
-        'fit_intercept':[a],
-        'normalize':[b],
-        'copy_X':[c],
-        'n_jobs':[d]
+        'fit_intercept':a,
+        'normalize':b,
+        'copy_X':c,
+        'n_jobs':d
         }
 
 def params_gradient_boosting(a,b,c,d,e,f,g,h,i,j):
     return {
-        "n_estimators": [a],
-        "learning_rate": [b],
-        "max_depth": [c], 
-        "min_samples_split": [d], 
-        "min_samples_leaf": [e], 
-        "max_features": [f], 
-        "subsample": [g], 
-        "criterion": [h], 
-        "random_state": [i], 
-        "verbose": [j]
+        "n_estimators": a,
+        "learning_rate": b,
+        "max_depth": c, 
+        "min_samples_split": d, 
+        "min_samples_leaf": e, 
+        "max_features": f, 
+        "subsample": g, 
+        "criterion": h, 
+        "random_state": i, 
+        "verbose": j
     }
+
+def data_test_train_split(file_path,y_reg,percent_split,shuffle_data):
+    df = pd.read_csv(file_path)
+    all_colums = [x for x in df.columns]
+    all_colums.remove(y_reg)
+    X = df[[x for x in all_colums]]
+    y = df[y_reg]
+
+    X_train,X_test,y_train,y_test = train_test_split(X,y,train_size=percent_split,shuffle=shuffle_data)
+
+    return X_train, X_test, y_train, y_test
+
+# нужно вывести ещё показатели
+def regression_model(model,X_train,X_test,y_train,y_test):
+    model.fit(X_train,y_train)
+    y_pred = model.predict(X_test)
+    mse = mean_squared_error(y_pred,y_test)
+    return mse
